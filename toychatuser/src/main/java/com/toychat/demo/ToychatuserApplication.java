6b5547803b3387cc447ca1dd0362e630fc9a1bbd
H2suLee <h2su0712@naver.com>
2024-08-02 09:47:32 +0900

    1차

---
+++ toychatuser/src/main/java/com/toychat/demo/ToychatuserApplication.java
@@ -0,0 +1,13 @@
+package com.toychat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToychatuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToychatuserApplication.class, args);
+	}
+
+}
